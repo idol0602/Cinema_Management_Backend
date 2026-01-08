7fb1fe69c443d2b64a6c8f0aa427c21a349b368a
Nguyen Nhat Quan <NguyenNhatQuan06022004@gmail.com>
2026-01-09 00:18:38 +0700

    00:18 9/1/2026

--- validators/movie_type.schema.js
+++ validators/movie_type.schema.js
@@ -3,6 +3,7 @@ import { z } from "zod";
 export const createMovieTypeSchema = z.object({
   type: z.string().min(1),
   created_at: z.string().default(new Date().toISOString()).optional(),
+  is_active: z.boolean().default(true).optional(),
 });
 
 export const updateMovieTypeSchema = createMovieTypeSchema.partial();
